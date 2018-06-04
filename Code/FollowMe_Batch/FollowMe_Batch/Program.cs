71b48d88440acbce6eb6d66d3927bd36ce92cd97
LeahRozenbach <leah1.roz@gmail.com>
2018-06-04 15:15:33 +0300

    Update Program.cs

--- Code/FollowMe_Batch/FollowMe_Batch/Program.cs
+++ Code/FollowMe_Batch/FollowMe_Batch/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Mail;
 
 namespace FollowMe_Batch
 {
@@ -17,11 +18,52 @@ namespace FollowMe_Batch
         {
             Console.ReadLine();
             FollowMeDBEntities fmEF = new FollowMeDBEntities();
-           List<ColorsSelect_Result> dt= fmEF.ColorsSelect().ToList();
-            
+            string massageLostDetailes = "";
+            foreach (var order in fmEF.Order.ToList())
+            {
+                string firstEPC = fmEF.OrderDetails.Where(x => x.OrderId == order.OrderId).First().FirstEPC;
+                string lastEPC = fmEF.OrderDetails.Where(x => x.OrderId == order.OrderId).First().LastEPC;
+                var ReadingForOrderId = fmEF.ReadingForOrderIdSelect(firstEPC, lastEPC).ToList();
+                if (ReadingForOrderId.Count > 1)//אם הכביסה נמצאה בפיזור של כמה תחנות
+                {
+                    //שרשור שם הלקוח ופרטי התחנות
+                    massageLostDetailes = " customer " + fmEF.Owners.Where(x => x.OwnerId == order.OwnerId).First().Name + " phone: " + fmEF.Owners.Where(x => x.OwnerId == order.OwnerId).First().Phone + "\n";
+                    massageLostDetailes += "lost detailes in stations: ";
+                    foreach (var item in ReadingForOrderId)
+                    {
+                        massageLostDetailes += item.Station + ",  ";
+                    }
+                    massageLostDetailes = massageLostDetailes.Substring(massageLostDetailes.LastIndexOf(","));
+                    massageLostDetailes += "\n";
+                    SendMail(massageLostDetailes);  //שליחת מייל
+                }
+            }
             //Application.EnableVisualStyles();
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
+        }
+
+        static void SendMail(string mailBody)
+        {
+            MailMessage mail = new MailMessage();
+            mail.To.Add("leah1.roz@gmail.com");
+            mail.From = new MailAddress("leah1.roz@gmail.com");
+            mail.Subject = "lost detailes";
+            mail.Body = mailBody;
+            mail.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Credentials = new System.Net.NetworkCredential(
+                               "leah1.roz@gmail.com", "12345678");
+            smtp.EnableSsl = true;
+            try
+            {
+                smtp.Send(mail);
+            }
+            catch (Exception ex)
+            {
+            }
+
         }
     }
 }
