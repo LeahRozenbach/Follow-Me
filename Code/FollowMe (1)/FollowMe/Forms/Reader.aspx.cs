49e3f184912dab995e2438e63eb95ec945b25eb7
LeahRozenbach <leah1.roz@gmail.com>
2018-06-21 09:21:25 +0300

    Update Reader.aspx.cs

--- Code/FollowMe (1)/FollowMe/Forms/Reader.aspx.cs
+++ Code/FollowMe (1)/FollowMe/Forms/Reader.aspx.cs
@@ -1,162 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
-using System.Data;
+<%@ Page Title="" Language="C#" MasterPageFile="~/Shared/Manager.Master" AutoEventWireup="true" CodeBehind="Reader.aspx.cs" Inherits="FollowMe.Forms.Reader" %>
 
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
-namespace FollowMe.Forms
-{
-    public partial class Reader : System.Web.UI.Page
-    {
-        Dal dal;
-        FollowMeDBEntities ef = new FollowMeDBEntities();
-        protected void Page_Load(object sender, EventArgs e)
-        {
-        }
+    <%--  <asp:ScriptManagerProxy ID="ScriptManagerProxy1" runat="server">
+    </asp:ScriptManagerProxy>--%>
+    <br />
+    <br />
+    <br />
+    <asp:Panel ID="Panel1" runat="server">
+        <asp:Label ID="Label1" runat="server" Text="Select a reader you want to edit / delete   "></asp:Label>
+        <br />
+        <br />
 
-        protected void Page_PreInit(object sender, EventArgs e)
-        {
-            if (Session["MasterPage"] != null)
-            {
-                MasterPage master = Session["MasterPage"] as MasterPage;
-                this.MasterPageFile = master.MasterPageFile;
-            }
-        }
-        private List<int> GetSelectedAntennaList()
-        {
-            System.Web.UI.WebControls.CheckBox[] antennaBoxes = { Ant1CheckBox, Ant2CheckBox, Ant3CheckBox, Ant4CheckBox };
-            List<int> ant = new List<int>();
-            for (int antIdx = 0; antIdx < antennaBoxes.Length; antIdx++)
-            {
-                System.Web.UI.WebControls.CheckBox antBox = antennaBoxes[antIdx];
-                if (antBox.Visible && antBox.Enabled && antBox.Checked)
-                {
-                    int antNum = antIdx + 1;
-                    ant.Add(antNum);
-                }
-            }
-            if (0 == ant.Count)
-            {
-                throw new ArgumentException("Please select at least one antenna");
-            }
-            return ant;
-        }
+        <asp:GridView ID="gvReaders" runat="server" CaptionAlign="Right" AllowPaging="True" PageSize="5"
+            AllowSorting="True" AutoGenerateColumns="False"
+            ForeColor="#333333" GridLines="None" DataKeyNames="ReaderId" DataSourceID="EntityDataSource1">
+            <%--DataSourceID="SqlDataSource1"--%>
+            <HeaderStyle CssClass="gridHeaderStyle" />
+            <PagerStyle BackColor="White" />
+            <RowStyle CssClass="gridRowStyle" />
+            <AlternatingRowStyle BackColor="White" />
+            <Columns>
+                <asp:CommandField ShowSelectButton="True" />
+                <asp:BoundField DataField="ReaderId" HeaderText="ReaderId" ReadOnly="True" SortExpression="ReaderId" />
+                <asp:BoundField DataField="Area" HeaderText="Area" SortExpression="Area" />
+                <asp:BoundField DataField="IP" HeaderText="IP" SortExpression="IP" />
+                <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+            </Columns>
+            <EditRowStyle CssClass="gridEditRowStyle" />
+            <FooterStyle CssClass="gridFooterStyle" />
+            <HeaderStyle ForeColor="White" />
+            <PagerStyle BackColor="White" />
+            <RowStyle BackColor="#EFF3FB" />
+            <SelectedRowStyle CssClass="gridSelectedRowStyle" BackColor="#4f81bd" />
 
+        </asp:GridView>
+        <asp:EntityDataSource ID="EntityDataSource1" runat="server" ConnectionString="name=FollowMeDBEntities" DefaultContainerName="FollowMeDBEntities" EnableDelete="True" EnableFlattening="False" EnableInsert="True" EnableUpdate="True" EntitySetName="Readers">
+        </asp:EntityDataSource>
+        <%--   <asp:SqlDataSource ID="SqlDataSource1" runat="server"
+            ConnectionString="<%$ ConnectionStrings:FollowMeDBEntities %>"
+            SelectCommand="SELECT [ReaderId], [Area], [IP], [Name] FROM [Readers]">
+        </asp:SqlDataSource>
+        --%>
+        <br />
+        <br />
+        <asp:LinkButton ID="btnEdit" runat="server" CssClass="LinkButton" OnClick="btnEdit_Click">Edit</asp:LinkButton>
 
-        protected void btnOk_Click(object sender, EventArgs e)
-        {
-            List<int> ant;
-            try
-            {
-                ant = GetSelectedAntennaList();
-                 ef.ReaderInsert(txtName.Text, txtArea.Text, txtIp.Text);
-                System.Web.UI.WebControls.CheckBox[] antennaBoxes = { Ant1CheckBox, Ant2CheckBox, Ant3CheckBox, Ant4CheckBox };
-                ef.ActiveAntennasInsert((Ant1CheckBox.Checked ? 1 : 0), (Ant2CheckBox.Checked ? 2 : 0), (Ant3CheckBox.Checked ? 3 : 0), (Ant4CheckBox.Checked ? 4 : 0));
-                ClientScript.RegisterStartupScript(GetType(),
-                   "completed", "<SCRIPT LANGUAGE='javascript'>alert(' action completed! ')</script>");
-            
-                if (Session["edit"] != null)
-                {
-                    DeleteReader(gvReaders.SelectedRow.Cells[1].Text);
-                    Session["edit"] = null;
-                }
-                ChangeVisible();
-                this.DataBind();
-            }
-            catch (ArgumentException ex)
-            {
-                ClientScript.RegisterStartupScript(GetType(),
-                  "error", "<SCRIPT LANGUAGE='javascript'>alert(' error in save ')</script>");
-            
-            }
-        }
+        <asp:LinkButton ID="btnDel" runat="server" CssClass="LinkButton" OnClick="btnDel_Click">Delete</asp:LinkButton>
 
-        protected void btnCancel_Click(object sender, EventArgs e)
-        {
-            ChangeVisible();
-        }
-
-        protected void btnEdit_Click(object sender, EventArgs e)
-        {
-            if (gvReaders.SelectedIndex == -1)
-            {
-                ClientScript.RegisterStartupScript(GetType(),
-                    "Error", "<SCRIPT LANGUAGE='javascript'>alert(' You  must choose reader to edit! ')</script>");
-            }
-            else
-            {
-                Session["edit"] = true;
-                txtName.Text = gvReaders.SelectedRow.Cells[2].Text;
-                txtArea.Text = gvReaders.SelectedRow.Cells[3].Text;
-                txtIp.Text = gvReaders.SelectedRow.Cells[4].Text;
-                ChangeVisible();
-                dal = new Dal();
-                DataTable dtAntennaAndReader;
-                int readerId = Convert.ToInt32(gvReaders.SelectedRow.Cells[1].Text);
-                var listConn= ef.ActiveAntenna.Where(x => x.ReaderId == readerId).Select(x => x.ActiveAntennas).ToList();
-                bool[] ant = { false, false, false, false };
-                foreach (var item in listConn)
-                {
-                ant[Convert.ToInt32(item.Value) - 1] = true;
-                }
-
-                Ant1CheckBox.Checked = ant[0];
-                Ant2CheckBox.Checked = ant[1];
-                Ant3CheckBox.Checked = ant[2];
-                Ant4CheckBox.Checked = ant[3];
-            }
-        }
-
-        protected void lnkNew_Click(object sender, EventArgs e)
-        {
-            txtArea.Text = "";
-            txtIp.Text = "";
-            txtName.Text = "";
-            Ant1CheckBox.Checked = false;
-            Ant2CheckBox.Checked = false;
-            Ant3CheckBox.Checked = false;
-            Ant4CheckBox.Checked = false;
-            ChangeVisible();
-        }
-
-
-        protected void btnDel_Click(object sender, EventArgs e)
-        {
-            if (gvReaders.SelectedIndex == -1)
-            {
-                ClientScript.RegisterStartupScript(GetType(),
-                    "Error", "<SCRIPT LANGUAGE='javascript'>alert(' You  must choose reader to edit! ')</script>");
-            }
-            else
-            {
-                DeleteReader(gvReaders.SelectedRow.Cells[1].Text);
-                gvReaders.DataBind();
-            }
-        }
-
-        protected void DeleteReader(string id)
-        {
-            ef.ReaderDelete(Convert.ToInt32(id));
-               }
-
-
-
-        protected void ChangeVisible()
-        {
-            foreach (System.Web.UI.Control item in Panel1.Controls)
-            {
-                try
-                {
-                    item.Visible = !(item.Visible);
-                }
-                catch { }
-            }
-
-        }
-
-    }
-}
+        <br />
+        <br />
+        <asp:LinkButton ID="lnkNew" runat="server" CssClass="LinkButton" OnClick="lnkNew_Click">New reader</asp:LinkButton>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Visible="false" Text=" Set a new reader!"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <asp:Table ID="tblPage" Visible="false" runat="server">
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblName" runat="server" Text="Name Reader:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br />
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblIp" runat="server" Text="Ip:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtIp" runat="server"></asp:TextBox>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" Display="Dynamic"
+                        ErrorMessage="You must enter data!" ControlToValidate="txtIp"></asp:RequiredFieldValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblActiveAntennas" runat="server" Text="ActiveAntennas:"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:PlaceHolder ID="phAntennas" runat="server">
+                        <asp:CheckBox ID="Ant1CheckBox" Text=" 1 " runat="server" />
+                        <asp:CheckBox ID="Ant2CheckBox" Text=" 2 " runat="server" />
+                        <asp:CheckBox ID="Ant3CheckBox" Text=" 3 " runat="server" />
+                        <asp:CheckBox ID="Ant4CheckBox" Text=" 4 " runat="server" />
+                    </asp:PlaceHolder>
+                </asp:TableCell>
+            </asp:TableRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:Label ID="lblArea" runat="server" Text="Area"></asp:Label>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:TextBox ID="txtArea" runat="server"></asp:TextBox>
+                </asp:TableCell>
+                <asp:TableCell>
+                    <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtArea"
+                        Display="Dynamic" ErrorMessage="You must enter data!"></asp:RequiredFieldValidator>
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <br />
+        <br />
+        <asp:Button ID="btnOk" runat="server" Text="Ok" Visible="false" CssClass="button" OnClick="btnOk_Click" />
+        &nbsp  &nbsp  &nbsp 
+  <br />
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="btnCancel" runat="server" Visible="false" CssClass="button" Text="Cancel"
+            OnClick="btnCancel_Click" />
+    </asp:Panel>
+</asp:Content>
